e260d4b9f280680849113dfb87f7903ded72dadb
evans <EvansGarihard@gmail.com>
2017-04-23 19:50:33 -0400

    added new gulp task and a dist directory

--- gulpfile.js
+++ gulpfile.js
@@ -10,64 +10,64 @@ var deploy = require('gulp-gh-pages');
 
 // Set the banner content
 var banner = ['/*!\n',
-    ' * Hockey Performances - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2017-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
-    ' */\n',
-    ''
+' * Hockey Performances - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+' * Copyright 2017-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
+' */\n',
+''
 ].join('');
 
 // Compile LESS files from /less into /css
 gulp.task('less', function() {
-    return gulp.src('less/hockeyperformances.less')
-        .pipe(less())
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+  return gulp.src('less/hockeyperformances.less')
+  .pipe(less())
+  .pipe(header(banner, { pkg: pkg }))
+  .pipe(gulp.dest('css'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
 });
 
 // Minify compiled CSS
 gulp.task('minify-css', ['less'], function() {
-    return gulp.src('css/hockeyperformances.css')
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+  return gulp.src('css/hockeyperformances.css')
+  .pipe(cleanCSS({ compatibility: 'ie8' }))
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest('css'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
 });
 
 // Minify JS
 gulp.task('minify-js', function() {
-    return gulp.src('js/hockeyperformances.js')
-        .pipe(uglify())
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('js'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+  return gulp.src('js/hockeyperformances.js')
+  .pipe(uglify())
+  .pipe(header(banner, { pkg: pkg }))
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest('js'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
 });
 
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
-    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
+  gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
+  .pipe(gulp.dest('vendor/bootstrap'))
 
-    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
+  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+  .pipe(gulp.dest('vendor/jquery'))
 
-    gulp.src([
-            'node_modules/font-awesome/**',
-            '!node_modules/font-awesome/**/*.map',
-            '!node_modules/font-awesome/.npmignore',
-            '!node_modules/font-awesome/*.txt',
-            '!node_modules/font-awesome/*.md',
-            '!node_modules/font-awesome/*.json'
-        ])
-        .pipe(gulp.dest('vendor/font-awesome'))
+  gulp.src([
+    'node_modules/font-awesome/**',
+    '!node_modules/font-awesome/**/*.map',
+    '!node_modules/font-awesome/.npmignore',
+    '!node_modules/font-awesome/*.txt',
+    '!node_modules/font-awesome/*.md',
+    '!node_modules/font-awesome/*.json'
+  ])
+  .pipe(gulp.dest('vendor/font-awesome'))
 })
 
 // Run everything
@@ -75,25 +75,40 @@ gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-            baseDir: ''
-        },
-    })
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  })
 })
 
 // Dev task with browserSync
 gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
-    gulp.watch('less/*.less', ['less']);
-    gulp.watch('css/*.css', ['minify-css']);
-    gulp.watch('js/*.js', ['minify-js']);
-    // Reloads the browser whenever HTML or JS files change
-    gulp.watch('*.html', browserSync.reload);
-    gulp.watch('js/**/*.js', browserSync.reload);
+  gulp.watch('less/*.less', ['less']);
+  gulp.watch('css/*.css', ['minify-css']);
+  gulp.watch('js/*.js', ['minify-js']);
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('js/**/*.js', browserSync.reload);
 });
+
+// Copy files to be published
+gulp.task('copy-to-dist', function() {
+  gulp.src(['img/**/*'])
+  .pipe(gulp.dest('dist/img'))
+
+  gulp.src(['css/*.min.css'])
+  .pipe(gulp.dest('dist/css'))
+
+  gulp.src(['js/*.min.js'])
+  .pipe(gulp.dest('dist/js'))
+  
+  gulp.src(['index.html'])
+  .pipe(gulp.dest('dist/'))
+})
 
 // Push build to gh-pages
 gulp.task('deploy', function () {
   return gulp.src("./dist/**/*")
-    .pipe(deploy())
+  .pipe(deploy())
 });
